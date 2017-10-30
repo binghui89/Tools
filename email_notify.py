b62580aa24c2b777c0d76ca854579f88f1b3c9e9
binghui89 <bli6@ncsu.edu>
2017-10-29 22:38:01 -0400

    Improve email notification function.

--- email_notify.py
+++ email_notify.py
@@ -1,12 +1,10 @@
-import smtplib  
-from email.mime.text import MIMEText  
-mailto_list=['bli6@ncsu.edu', 'reagan.fruit@gmail.com'] 
+import smtplib, sys
+from email.mime.text import MIMEText
 mail_host="smtp.gmail.com:587"  # SMTP server
 mail_user="reagan.fruit"    # Username
-mail_pass=   # Password, fill it out each time you use it
 mail_postfix="gmail.com"  # 
   
-def send_mail(to_list,sub,content):  
+def send_mail(to_list, sub, content, mail_pass):  
     # For help go to the following link:
     # https://www.digitalocean.com/community/questions/unable-to-send-mail-through-smtp-gmail-com
     me="Binghui Li"+"<"+mail_user+"@"+mail_postfix+">"  
@@ -27,8 +25,21 @@ def send_mail(to_list,sub,content):
         return False  
 
 if __name__ == '__main__':  
-    # mailto_list = 'bli6@ncsu.edu'
-    if send_mail(mailto_list,"Model run completed.","The model run is completed!"):
-        print "Email sent successfully!"  
-    else:  
-        print "Email sent failed."  
+    mailto_list = ['reagan.fruit@gmail.com']
+    if len(sys.argv) > 1:
+        mail_pass = sys.argv[1]
+    else:
+        mail_pass = None
+
+    if mail_pass:
+        if send_mail(
+            mailto_list,
+            "Model run completed.",
+            "The model run is completed!",
+            mail_pass
+        ):
+            print "Email sent successfully!"  
+        else:  
+            print "Email sent failed."
+    else:
+        print "No password!\nEmail sent failed."
